963359f9c3908c247b3ba959ca125b02b57f3d55
Mateus Rossatto <mateusrossatto@gmail.com>
2020-01-17 16:49:05 -0300

    Add a contains function to cards.ex

--- lib/cards.ex
+++ lib/cards.ex
@@ -18,4 +18,20 @@ defmodule Cards do
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  
 end
